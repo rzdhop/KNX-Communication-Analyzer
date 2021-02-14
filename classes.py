b72c0dd7184c624d121535a951bba31187714fd6
Guillaume Dorschner <Guillaume.dorschner@gmail.com>
2021-02-15 00:16:51 +0100

    feat: telegram cut

--- classes.py
+++ classes.py
@@ -2,18 +2,53 @@
 import driver
 
 
+def cut(trameBruteKNX):
+    """cut of the telegram as a carataire"""
+    tout = ""
+    champ = ""
+    pariter = 0
+    for x in range(len(trameBruteKNX)):
+        # Start
+        if(x % 13 == 0):
+            champ = ""
+            pariter = 0
+        # Parity
+        elif(x % 13 == 9):
+            pariter += int(trameBruteKNX[x])
+            if not (pariter % 2):
+                tout += champ
+        # 1 Stop and 2 Pause
+        elif(x % 13 == 10 or x % 13 == 11 or x % 13 == 12):
+            pass
+        # Data
+        else:
+            champ = str(champ) + str(trameBruteKNX[x])
+            if(trameBruteKNX[x] == "1"):
+                pariter += 1
+
+    return hex(int(tout[::-1], 2))
+
+
 class CTrameKNX:
     def __init__(self, trameBruteKNX: str = None):
         if trameBruteKNX == None:
-            # constuceur simple
-            print("🤷‍♂️")
+            # Simple constructor
+            print("🤷‍♂️ Yes But Why ?")
         else:
-            # constructeur surcharger avec la trame
+            # Constructor overload with the trame
             self.trameBruteKNX = trameBruteKNX
+            self.octetControle = ""
+            self.adresseSoucre = ""
+            self.adresseDestinataire = ""
+            self.CR = ""
+            self.LG = ""
+            self.Data = ""
+            self.securite = ""
         pass
 
-    def checkEmissionNormaleOuReception(self):
-        pass
+    def traitement(self):
+
+        print(cut(self.trameBruteKNX))
 
     def calculerChecksum(self):
         calcul = 1 + 1
