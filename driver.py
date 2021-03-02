b4e6e5aef940722a7872c6693ed66d5fee4a725f
Osaedasia <75844741+Osaedasia@users.noreply.github.com>
2021-03-02 14:54:29 +0100

    Update driver.py
    Ajouts de nouvelles fonctions

--- driver.py
+++ driver.py
@@ -10,12 +10,28 @@ import serial
 from serial import Serial
 
 
-def driverCap(userPort,nbBytes = 16):
+def getListesPorts():
+    ports = serial.tools.list_ports.comports() #Permet de stocker dans un liste tout les ports qui sont connéctés
+    
+    return ports
+
+def findKnxPort(ports):
+    portCOM = 'Erreur aucun port correspondant'
+    listePorts = len(ports)
+
+    for i in range (0, listePorts):
+        port = ports [i]
+        strPort = str(port) 
+
+        if 'nom du périphe' in strPort: #Je procéde à une vérification par un mot clés
+            splitPort = strPort.split(' ') #Ici je sépare strPort à chaques éspaces
+            portCOM = (splitPort[0]) #J'assigne à portCOM le premier champs de splitPort qui correspond au COM du port
+
+    return portCOM
+
+
+def driverCap(userPort):
     capKNX = serial.Serial(userPort, 9600, timeout= 1) # ici j'ouvre et configure le port série, en lui précisant le port sélectionner, puis le taux de bauds pour le port et je lui défini un timeout
-    dataKNX = capKNX.readline(nbBytes) # ici je vais lire les données sur le port pour une durée de nbBytes 
-
+    dataKNX = capKNX.readline() # ici je vais lire les données sur le port jusqu'a un EOL characters
+   
     return dataKNX
-
-
-print(driverCap('COM1',16))
-l
