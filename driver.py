266cf7c7efe51ba93f795f58ddceead769286cbe
Osaedasia <75844741+Osaedasia@users.noreply.github.com>
2021-03-03 13:25:19 +0100

    Driver fini (en théorie)
    Transformation en class

--- driver.py
+++ driver.py
@@ -1,5 +1,5 @@
 '''
-Created on 23-02-2021
+Created on 03-03-2021
 
 @author: Thibaud
 '''
@@ -7,31 +7,39 @@ Created on 23-02-2021
 
 #J'importe les packets de pyserial
 import serial
-from serial import Serial
+import serial.tools.list_ports # import de la biblio pour recup les ports
 
+class driverKNX:
+    def __init__(self):
+        self.ports = ''
+        self.portCOM = ''
 
-def getListesPorts():
-    ports = serial.tools.list_ports.comports() #Permet de stocker dans un liste tout les ports qui sont connéctés
+        self.getListesPorts() #Permet de lancer au démarrage sans l'appeler
+        self.findKnxPort()
+
     
-    return ports
+    def getListesPorts(self):
+        self.ports = serial.tools.list_ports.comports() #Permet de stocker dans un liste tout les ports qui sont connéctés
+        
+    def findKnxPort(self):
+        self.portCOM = 'Erreur'
+        listePorts = len(self.ports)
 
-def findKnxPort(ports):
-    portCOM = 'Erreur aucun port correspondant'
-    listePorts = len(ports)
+        for i in range (0, listePorts):
+            port = self.ports [i]
+            strPort = str(port) 
 
-    for i in range (0, listePorts):
-        port = ports [i]
-        strPort = str(port) 
+            if 'Prolific' in strPort: #Je procéde à une vérification par un mot clés
+                splitPort = strPort.split(' ') #Ici je sépare strPort à chaques éspaces
+                self.portCOM = (splitPort[0]) #J'assigne à portCOM le premier champs de splitPort qui correspond au COM du port
+            else:
+                print('Erreur aucun port trouver')    
 
-        if 'nom du périphe' in strPort: #Je procéde à une vérification par un mot clés
-            splitPort = strPort.split(' ') #Ici je sépare strPort à chaques éspaces
-            portCOM = (splitPort[0]) #J'assigne à portCOM le premier champs de splitPort qui correspond au COM du port
+    def driverCap(self):
 
-    return portCOM
+        capKNX = serial.Serial(self.portCOM, 9600, timeout= 1) # ici j'ouvre et configure le port série, en lui précisant le port sélectionner, puis le taux de bauds pour le port et je lui défini un timeout
 
+        dataKNX = capKNX.readline() # ici je vais lire les données sur le port jusqu'a un EOL characters
 
-def driverCap(userPort):
-    capKNX = serial.Serial(userPort, 9600, timeout= 1) # ici j'ouvre et configure le port série, en lui précisant le port sélectionner, puis le taux de bauds pour le port et je lui défini un timeout
-    dataKNX = capKNX.readline() # ici je vais lire les données sur le port jusqu'a un EOL characters
+        return dataKNX.decode('utf-8')
    
-    return dataKNX
