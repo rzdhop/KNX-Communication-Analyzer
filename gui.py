02a4645132f7fef35f963e25f7a0899280e5a0c3
Kimd999 <75373547+Kimd999@users.noreply.github.com>
2021-04-13 19:19:26 +0200

    Update gui.py
    Add Tkinter Interface

--- gui.py
+++ gui.py
@@ -1,127 +1,147 @@
 # Kwin
-import sys
-import os
 
-from PyQt5 import QtWidgets, QtGui, QtCore
-#from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import * 
-from PyQt5.QtGui import *
+from tkinter import *
+from tkinter import ttk 
+from PIL import Image, ImageTk 
+from webbrowser import *
+from affitrame import *
 
-"""app = QApplication(sys.argv)"""
+#Fenêtre principale 
+class Fenetre_principale(Frame) :
 
-class Fenetre_secondaire(QMainWindow): #Class for the second window
-    #def __init__(self, parent = None): 
-    def __init__(self):
-        #super()._init_(parent)
-        super().__init__()
-    #Cette fenêtre sera celle où seront affichées les trames 
-        self.setWindowTitle("Analyseur de trames KNX")
-        self.resize(400,200)
-        print("Ici seront affichées les trames")
+    def __init__(self, parent): 
+        Frame.__init__(self, parent)
+
+        self.parent = parent 
+        
+        self.parent.title("Analyseur de trames KNX")
+
+        self.pack(fill = BOTH)
+
+        #Création du menu de la fenêtre
+
+        menu = Menu(self.parent)
+        self.parent.config(menu=menu)
+
+        menuOption = Menu(menu)
+        menuOption.add_checkbutton(label = "Afficher les logs", command = self.log)
+        menuOption.add_checkbutton(label = "Mode sombre", command = self.darkmode)
+        menuOption.add_command(label = "Quitter", command = self.exitProgram)
+        menu.add_cascade(label = "Options", menu = menuOption)
+
+        menuAPropos = Menu(menu)
+        menuAPropos.add_command(label="Voir de l'aide")
+        menuAPropos.add_command(label="Afficher notre Github", command = self.github)
+        menu.add_cascade(label="À Propos", menu=menuAPropos)
+
+        #Création de la Combobox
+
+        self._initUI()
+
+    def exitProgram(self):
+        exit()
+
+    def log(self) :
+        print("Afficher les logs")
+
+    def darkmode(self) :
+        print("Afficher le mode sombre")
 
 
-class Fenetre_principale(QMainWindow):
-    #Class for the main window
 
-    def __init__(self, parent = None):
-        # Class Constructor
-        super().__init__(parent)
-        self.setWindowTitle("Analyseur de trame KNX")
-        self.setWindowIcon(QtGui.QIcon("C:\PROJET ANALYSEUR DE TRAMES KNX\KNX.png"))
-        self.resize(400, 200)
-        self.centralWidget = QLabel("Voici l'analyseur de trames KNX")
-        self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) 
-        self.setCentralWidget(self.centralWidget)
-        self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        self.setCentralWidget(self.centralWidget)
-        self._createMenuBar()
-        self._createToolBars()
-        self._createCombobox()
-        self._openNewWindow()
-        self._secondWindow()
+
+#Création de la fonction qui permettra à l'utilisateur d'aller regarder le Github 
+
+    def github(self) :
+        print("Afficher notre GitHub")
+
+        fengit = Toplevel(self)
+        self.pack(fill = BOTH)
+
+        text = Label(fengit, text = "Voici le lien de notre Github", font =("Impact", 12))
+        text.pack()
+
+        global git_img
+        git_img = Image.open("github_PNG19.png")
+        git_img = git_img.resize((150,150))
+        git_img = ImageTk.PhotoImage(git_img)
+        
+        git_logo = Label(fengit, image = git_img)
+        git_logo.pack()
+
+        #Création d'un lien hypertexte 
+
+        lien = Label(fengit, text = r"https://github.com/GuillaumeDorschner/KNX-Communication-Analyzer", fg = "blue", cursor = "arrow")
+        lien.pack()
+
+
+
+
         
 
-
-    def _createCombobox(self):
-        combo = QComboBox(self)
-        combo.setGeometry(QRect(40, 40, 200, 31))
-        liste = ["Veuillez choisir un port", "Port 1", "Port 2"]
-        combo.addItems(liste)
-        combo.move(50, 50)
-        self.qlabel = QLabel(self)
-        self.qlabel.move(50,15)
-        self.qlabel.setAlignment(Qt.AlignHCenter)
-              
-
-    def _createToolBars(self):
-        #Creating a toolbar function
-        fileToolBar = self.addToolBar("Fichier")
-        # Using a QToolBar object
-        editToolBar = QToolBar("Édition", self) 
-        self.addToolBar(editToolBar)
-        # Using a QToolBar object and a toolbar area
-        helpToolBar = QToolBar("Aide", self)
-        self.addToolBar(Qt.LeftToolBarArea, helpToolBar)
-
-    def _createMenuBar(self):
-
-        #Create a menubar function
-        menuBar = self.menuBar()
-        # Creating menus using a QMenu object
-        fileMenu = QMenu("&Fichier", self)
-        menuBar.addMenu(fileMenu)
-        # Creating menus using a title
-        editMenu = menuBar.addMenu("&Édition")
-        #helpMenu = menuBar.addMenu("&Help")
-        helpMenu = menuBar.addMenu(QIcon(""), "&Aide")
-
-    def _openNewWindow(self): 
-        button = QPushButton("Cliquez ici", self)
-        button.move(275,50)
-        button.setToolTip("Appuyez ici pour pouvoir afficher les trames")
-        button.clicked.connect(self._secondWindow)
-         
-
-    def _onChanged(self, text):
-        self.qlabel.setText(text)
-        self.qlabel.adjustSize()
-
-    def _secondWindow(self):
-        self.f = Fenetre_secondaire()
-        self.f.hide()
-        self.f.show()
+        
         
 
+#Fonction pour les widgets de la première fenêtre 
+    def _initUI(self):
+
+        self.pack(fill = BOTH)
+
+        vide = Label(self, text = " ")
+        vide.pack()
 
 
-def main() : 
-    app = QApplication(sys.argv)
-    fenetre = Fenetre_principale()
-    fenetre.show()
-    sys.exit(app.exec_())
+        select_port = ttk.Combobox(self, values = ["Veuillez choisir un port", "Port COM"])
+        select_port.pack()
+
+        vide = Label(self, text = " ")
+        vide.pack()
+
+
+        can = Canvas(self, width = 400, height = 400, background = "white")
+        #can.pack(side = LEFT)
+        can.pack()
+
+        global start_img
+        start_img = Image.open("Start_logo.png")
+        start_img = start_img.resize((50,50))
+        #start_img = start_img.resize((25,25), Image.ANTIALIAS)
+        start_img = ImageTk.PhotoImage(start_img)
+
 
         
-if __name__ == "__main__":
+        global pause_img
+        pause_img = Image.open("pause-button-clipart-4.jpg")
+        pause_img = pause_img.resize((50,50))
+        #pause_img = pause_img.resize((25,25), Image.ANTIALIAS)
+        pause_img = ImageTk.PhotoImage(pause_img)
+        
+
+        boutonStart = Button(self, text = "Démarrer", image = start_img, relief = "flat", command = self.startAnalyse)
+        #boutonStart.pack(side = BOTTOM)
+        boutonStart.pack(side = LEFT)
+
+        boutonPause = Button(self, text = "Pause", image = pause_img, relief = "flat", command = self.stopAnalyse)
+        #boutonPause.pack()
+        boutonPause.pack(side = RIGHT)
+
+    def startAnalyse(self):
+        print("Lancer l'analyseur de trames")
+
+    def stopAnalyse(self):
+        print("Stopper l'analyseur de trames")
+
+
+   
+
+      
+
+def main():
+  
+    f1 = Tk()
+    f1.resizable(width = 1000, height = 1000)
+    app = Fenetre_principale(f1)
+    f1.mainloop()  
+
+if __name__ == '__main__':
     main()
-
-
- 
-    #def _onClicked(self, val):
-        #Check if any item is selected or not 
-        #if val == "Choissisez votre port":
-         #   message = "Vous n'avez rien selectionné"
-        
-        #else : 
-         #   message = "Vous avez selectionné le port" + val 
-
-        #self.msg"""
-
-
-
-
-
-
-
-
-
